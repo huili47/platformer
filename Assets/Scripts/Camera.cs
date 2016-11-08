5ebd1dd08743910d59daf107962728d95c995581
huili47 <huili47@gmaill.com>
2016-11-08 17:49:40 -0500

    Starting Commit

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    private Player player;
+    private Vector3 offset;
+
+	void Start () {
+        player = FindObjectOfType<Player>();
+        offset = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          transform.position = player.transform.position + offset;
+   
+	}
+}
---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour {
+    public int coins = 0;
+    public int lives = 3;
+
+    public Text coinsText;
+    public Text livesText;
+
+    public GameObject winSign;
+    public GameObject loseSign;
+
+    // Use this for initialization
+    void Start () {
+        coinsText.text = coins.ToString();
+        livesText.text = lives.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CoinWasPickedUp(int worth)
+    {
+        coins+=worth;
+        coinsText.text = coins.ToString();
+    }
+
+    public void LifeWasLost()
+    {
+        lives--;
+        livesText.text = lives.ToString();
+
+        if(lives==0)
+        {
+            loseSign.SetActive(true);
+
+        }
+    }
+}
